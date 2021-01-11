b18f02b200d2dd1012ac01d1e8288455f26d2863
potchi23 <53357059+potchi23@users.noreply.github.com>
2021-01-11 15:20:52 +0100

    indentacion

--- www/init.php
+++ www/init.php
@@ -2,44 +2,42 @@
 
 class Application{
 
-        private static $instance;
-        private $conn = false;
-        private function __construct(){}
-        
-        public static function getInstance(){
-            if (!self::$instance instanceof self) {
-                self::$instance = new self;
-		    }
-                
-            return self::$instance;
-        }
-        
-        
-        public function init(){
-		if (!$this->conn ) {
+    private static $instance;
+    private $conn = false;
+    private function __construct(){}
+    
+    public static function getInstance(){
+        if (!self::$instance instanceof self) {
+            self::$instance = new self;
+	    }
             
+        return self::$instance;
+    }
+    
+    
+    public function init(){
+	    if (!$this->conn ) {
             /*
             $dbHost = "fdb28.awardspace.net";
-			$dbUser = "3648978_db"; 
+		    $dbUser = "3648978_db"; 
             $dbName = "3648978_db";
-			$dbPass = "m3l!;Jd_1P!CSa9(";
+		    $dbPass = "m3l!;Jd_1P!CSa9(";
             * */
-
             $dbHost = "localhost";
             $dbName = "messages_db";
-			$dbUsername = "root"; 
-			$dbPassword = "";
-			
-			$this->conn = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
-			if ( $this->conn->connect_errno ) {
-				echo "Error de conexión a la BD: (" . $this->conn->connect_errno . ") " . utf8_encode($this->conn->connect_error);
-				exit();
-			}
-			if ( ! $this->conn->set_charset("utf8mb4")) {
-				echo "Error al configurar la codificación de la BD: (" . $this->conn->errno . ") " . utf8_encode($this->conn->error);
-				exit();
-			}
-		}
+		    $dbUsername = "root"; 
+		    $dbPassword = "";
+            
+		    $this->conn = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+		    if ( $this->conn->connect_errno ) {
+		    	echo "Error de conexión a la BD: (" . $this->conn->connect_errno . ") " . utf8_encode($this->conn->connect_error);
+		    	exit();
+		    }
+		    if ( ! $this->conn->set_charset("utf8mb4")) {
+		    	echo "Error al configurar la codificación de la BD: (" . $this->conn->errno . ") " . utf8_encode($this->conn->error);
+		    	exit();
+		    }
+	    }
 		
 		return $this->conn;
 	}
